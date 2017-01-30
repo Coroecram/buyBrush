7a852a76468bede0c9942fe9fbd409b3574ed73e
Michael Budnick <michaeldbudnick@gmail.com>
2017-01-30 12:13:10 -0500

    Add CSS and JS static files

--- src/buyBrush/urls.py
+++ src/buyBrush/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
-from sjango.conf.urls.static import static
+from django.conf.urls.static import static
 
 from profiles import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.about, name='about'),
 ]
 
 if settings.DEBUG:
