a6c6be47587b3850989e2e18921d4ec89f44013c
Michael Budnick <michaeldbudnick@gmail.com>
2017-01-31 13:48:21 -0500

    Settings test

--- src/buyBrush/settings.py
+++ src/buyBrush/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ddd
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
--- src/contact/views.py
+++ src/contact/views.py
@@ -6,7 +6,7 @@ from .forms import contactForm
 def contact(request):
 	form = contactForm(request.POST or None)
 	if form.is_valid():
-		print request.POST
+		print form.cleaned_data['email']
 	context = locals()
 	template = 'contact.html'
 	return render(request, template, context)
