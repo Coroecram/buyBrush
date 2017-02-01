a5affacaeab1087b2b8985fa5fbfbaef8e29755b
Michael Budnick <michaeldbudnick@gmail.com>
2017-02-01 17:50:36 -0500

    Add crip form

--- src/contact/forms.py
+++ src/contact/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 class contactForm(forms.Form):
     name = forms.CharField(required=False, max_length=100, help_text='100 characters max')
+    subject = forms.CharField(required=True, max_length=100, help_text='100 characters max')
     email = forms.EmailField(required=True)
     comment = forms.CharField(required=True, widget=forms.Textarea)
--- src/contact/views.py
+++ src/contact/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
 
 from .forms import contactForm
 
+
 # Create your views here.
 def contact(request):
+	title = 'Contact'
 	form = contactForm(request.POST or None)
+	confirm_message = None
+
 	if form.is_valid():
-		print form.cleaned_data['email']
-	context = locals()
+		formData = form.cleaned_data
+		subject = formData['subject']
+		message = formData['comment']
+		name = formData['name']
+		emailFrom = formData['email']
+		emailTo = [settings.EMAIL_HOST_USER]
+		send_mail( subject, message, emailFrom, emailTo, fail_silently=True )
+		title = "Thanks!"
+		form = None
+		confirm_message = "Thanks for the message, we will get right back to you!"
+
+	context = {'title': title, 'form': form, 'confirm_message': confirm_message, }
 	template = 'contact.html'
 	return render(request, template, context)
