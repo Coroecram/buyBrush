38007250579c40f76a333448d96ea9f2d3d0ba9a
Michael Budnick <michaeldbudnick@gmail.com>
2017-02-13 17:55:47 -0500

    All-auth and login on main page

--- src/buyBrush/urls.py
+++ src/buyBrush/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,7 +25,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', profile_views.home, name='home'),
     url(r'^about/$', profile_views.about, name='about'),
+    url(r'^profile/$', profile_views.userProfile, name='profile'),
     url(r'^contact/$', contact_views.contact, name='contact'),
+    url(r'^accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
